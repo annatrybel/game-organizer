aff4b5c5aaa8d471f7485a57962d8a664778fa20
Anna Trybel <annakalinovich717@gmail.com>
2026-03-01 14:53:08 +0100

    Add role seeding functionality with RoleSeeder and SeedManager classes

---
+++ GameOrganizer.Api/Seeders/SeedManager.cs
@@ -0,0 +1,17 @@
+﻿namespace GameOrganizer.Api.Seeders
+{
+    public class SeedManager
+    {
+        private readonly RoleSeeder _roleSeeder;
+
+        public SeedManager(RoleSeeder roleSeeder)
+        {
+            _roleSeeder = roleSeeder;
+        }
+
+        public async Task Seed()
+        {
+            await _roleSeeder.SeedRolesAsync();
+        }
+    }
+}
