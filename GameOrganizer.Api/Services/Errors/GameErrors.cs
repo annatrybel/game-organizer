eadb5d899de8ed53d3d2c41fc7656e8cf58c204c
Anna Trybel <annakalinovich717@gmail.com>
2026-03-07 15:48:46 +0100

    Add GameErrors class for handling game-related error messages

---
+++ GameOrganizer.Api/Services/Errors/GameErrors.cs
@@ -0,0 +1,10 @@
+﻿namespace GameOrganizer.Api.Services.Errors
+{
+    public static class GameErrors
+    {
+        private static readonly string objectName = "Game";
+
+        public static ServiceError GameAlreadyInCollection() => new ServiceError(
+            $"{objectName}.AlreadyInCollection", "Ta gra znajduje się już w Twojej kolekcji.");
+    }
+}
