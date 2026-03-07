f329d33933d8ea22f62f8272066339e992ebb2bf
Anna Trybel <annakalinovich717@gmail.com>
2026-03-07 15:48:25 +0100

    Add GameSeeder and integrate it into SeedManager for game data seeding

--- GameOrganizer.Api/Seeders/SeedManager.cs
+++ GameOrganizer.Api/Seeders/SeedManager.cs
@@ -4,17 +4,20 @@
     {
         private readonly RoleSeeder _roleSeeder;
         private readonly GenreSeeder _genreSeeder;
+        private readonly GameSeeder _gameSeeder;
 
-        public SeedManager(RoleSeeder roleSeeder, GenreSeeder genreSeeder)
+        public SeedManager(RoleSeeder roleSeeder, GenreSeeder genreSeeder, GameSeeder gameSeeder)
         {
             _roleSeeder = roleSeeder;
             _genreSeeder = genreSeeder;
+            _gameSeeder = gameSeeder;
         }
 
         public async Task Seed()
         {
             await _roleSeeder.SeedRolesAsync();
             await _genreSeeder.SeedGenresAsync();
+            await _gameSeeder.SeedGamesAsync();
         }
     }
 }
