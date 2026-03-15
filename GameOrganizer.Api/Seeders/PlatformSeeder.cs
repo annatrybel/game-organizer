31a38c172f55edaee6816b5534343e9093437e20
Anna Trybel <annakalinovich717@gmail.com>
2026-03-15 18:25:19 +0100

    Created PlatformSeeder to populate initial platform data.

---
+++ GameOrganizer.Api/Seeders/PlatformSeeder.cs
@@ -0,0 +1,27 @@
+﻿using GameOrganizer.Api.Models;
+using GameOrganizer.Api.Models.DatabaseModels;
+
+namespace GameOrganizer.Api.Seeders
+{
+    public class PlatformSeeder
+    {
+        private readonly GameOrganizerDbContext _context;
+
+        public PlatformSeeder(GameOrganizerDbContext context) => _context = context;
+
+        public async Task SeedPlatformsAsync()
+        {
+            if (!_context.Platforms.Any())
+            {
+                _context.Platforms.AddRange(
+                    new Platform { Name = "PC" },
+                    new Platform { Name = "PS5" },
+                    new Platform { Name = "Xbox Series X" },
+                    new Platform { Name = "Nintendo Switch" },
+                    new Platform { Name = "PS4" }
+                );
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- GameOrganizer.Api/Seeders/SeedManager.cs
+++ GameOrganizer.Api/Seeders/SeedManager.cs
@@ -5,12 +5,14 @@
         private readonly RoleSeeder _roleSeeder;
         private readonly GenreSeeder _genreSeeder;
         private readonly GameSeeder _gameSeeder;
+        private readonly PlatformSeeder _platformSeeder;
 
-        public SeedManager(RoleSeeder roleSeeder, GenreSeeder genreSeeder, GameSeeder gameSeeder)
+        public SeedManager(RoleSeeder roleSeeder, GenreSeeder genreSeeder, GameSeeder gameSeeder, PlatformSeeder platformSeeder)
         {
             _roleSeeder = roleSeeder;
             _genreSeeder = genreSeeder;
             _gameSeeder = gameSeeder;
+            _platformSeeder = platformSeeder;
         }
 
         public async Task Seed()
@@ -18,6 +20,7 @@
             await _roleSeeder.SeedRolesAsync();
             await _genreSeeder.SeedGenresAsync();
             await _gameSeeder.SeedGamesAsync();
+            await _platformSeeder.SeedPlatformsAsync();
         }
     }
 }
