636e5438c04db9988d2bce3259b760d8363603e6
Anna Trybel <annakalinovich717@gmail.com>
2026-03-25 15:49:07 +0100

    Added UserSearchResultDto with relationship status mapping

---
+++ GameOrganizer.Api/Models/Dto/UserSearchResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameOrganizer.Api.Models.Dto
+{
+    public class UserSearchResultDto
+    {
+        public string Id { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string? AvatarUrl { get; set; }
+        public string RelationStatus { get; set; } = "None";
+    }
+}
