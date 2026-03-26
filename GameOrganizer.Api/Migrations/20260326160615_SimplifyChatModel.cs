f50ea0a7c502d49b02749debbbfdc83dbf1143b7
Anna Trybel <annakalinovich717@gmail.com>
2026-03-26 17:31:33 +0100

    Removed redundant ReceiverId and Receiver relationship from ChatMessage

---
+++ GameOrganizer.Api/Migrations/20260326160615_SimplifyChatModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameOrganizer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SimplifyChatModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_AspNetUsers_ReceiverId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChatMessages_ReceiverId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiverId",
+                table: "ChatMessages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiverId",
+                table: "ChatMessages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_ReceiverId",
+                table: "ChatMessages",
+                column: "ReceiverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_AspNetUsers_ReceiverId",
+                table: "ChatMessages",
+                column: "ReceiverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
